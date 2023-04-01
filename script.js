02c2013fcfb76ae517672498077974ff233fc97a
uttamshukla259 <117875151+uttamshukla259@users.noreply.github.com>
2023-04-01 21:12:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // Your script here.
+// Populate the voices dropdown
+function populateVoices() {
+  const voices = speechSynthesis.getVoices();
+  voicesDropdown.innerHTML = voices
+    .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+    .join('');
+}
+
+// Start speaking
+function speak() {
+  // Create a new SpeechSynthesisUtterance object
+  const utterance = new SpeechSynthesisUtterance(textarea.value);
+
+  // Set the selected voice
+  const selectedVoice = voicesDropdown.value;
+  const voices = speechSynthesis.getVoices();
+  const voice = voices.find(v => v.name === selectedVoice);
+  utterance.voice = voice;
+
+  // Set the rate and pitch
+  utterance.rate = rate.value;
+  utterance.pitch = pitch.value;
